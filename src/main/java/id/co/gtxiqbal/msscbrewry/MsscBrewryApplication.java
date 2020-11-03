b24258961f39f5eb4e39ae57a711dec979a96ee6
iqbal.alhusain <moh.iqbal.alhusain@gmail.com>
2020-11-03 12:47:00 +0700

    first commit

---
+++ src/main/java/id/co/gtxiqbal/msscbrewry/MsscBrewryApplication.java
@@ -0,0 +1,13 @@
+package id.co.gtxiqbal.msscbrewry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/gtxiqbal/msscbrewry/MsscBrewryApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.gtxiqbal.msscbrewry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
